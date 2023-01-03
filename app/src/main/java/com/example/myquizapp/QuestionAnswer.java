84155d907f60a946cf0f0f8723bec760683bfd5f
Jose Sagwe <Josephsagwe421@gmail.com>
2023-01-03 16:54:05 +0300

    first commit

---
+++ app/src/main/java/com/example/myquizapp/QuestionAnswer.java
@@ -0,0 +1,85 @@
+package com.example.myquizapp;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "Which company owns the Android?",
+            "OS computer abbreviation usually means?",
+            "In which decade did the first transatlantic radio broadcast occur?",
+            "What is the part of a database that holds only one type of information?",
+            "Which is a type of Electrically-Erasable programmable Read-Only Memory?",
+            "What is the purpose of a choke in tube light?",
+            "What does HTML stand for?",
+            "Which car brand used the slogan Sheer Driving Pleasure?",
+            "Which one of the following is not a pick-up truck?",
+            "What does Volkswagen mean?",
+            "Which country is Honda from?",
+            "Which car manufacturer has a logo with four interlocking ring?",
+            "How many dots are on the Bugatti?",
+            "Who is the CEO of Tesla?",
+            "What is Ford Slogan?",
+            "Which year was the Porsche Cayenne launched?",
+            "Which country is Jaguar manufactured?",
+            "Which is the richest company in the world?",
+            "Which Country won FIFA world cup 2018?",
+            "What Does API stand for?",
+            "Who owns twitter",
+            "Which country is the Origin of Ticktock?",
+
+
+
+
+    };
+
+    public static String choices [][] = {
+            {"Google","Apple","Nokia","Samsung"},
+            {"Order of Significance","Operating System","Open Software","Optical Sensor"},
+            {"1850s","1860s","1870s","1900s"},
+            {"Report","Field","Record","File"},
+            {"Flash","Flange","Fury","FRAM"},
+            {"Decrease the light","Increase the light","Decrease Voltage","Increase Voltage"},
+            {"Hyper Trainer Markup Language","HyperTent Marketing Language","Hypertext Markup Language","HyperText Markup Leveler"},
+            {"Audi","Volvo","Ferrari","BMW"},
+            {"Ford Ranger","Peugeot Paltner","Nissan Navara","SsangYong Musso"},
+            {"Powerful Car","Beautiful Car","My Car","Peoples Car"},
+            {"China","Japan","Germany","England"},
+            {"Toyota","Audi","Isuzu","BMW"},
+            {"50 dots","60 dots","10 dots","5 dots"},
+            {"Elon Musk","Kiichiro Toyoda","Jack Ma Yun","Bil-gates"},
+            {"Go Further","Go Forward","Do anything","Above and Beyond"},
+            {"2002","1990","2006","2014"},
+            {"America","England","India","Japan"},
+            {"Youtube","Amazon","Apple","Waltmart"},
+            {"Germany","Argentina","France","Croatia"},
+            {"Artificial Personal Intelligence","Artificial Personal Interface","Application Programming interface","Application Intelligence "},
+            {"Elon Musk","Mark Z.","Jack Ma Yun","Bil-gates"},
+            {"America","Canada","China","England"},
+
+    };
+
+    public static String correctAnswer[] = {
+            "Google",
+            "Operating System",
+            "1900s",
+            "Field",
+            "Flash",
+            "Increase Voltage",
+            "Hypertext Markup Language",
+            "BMW",
+            "Peugeot Paltner",
+            "Peoples Car",
+            "Japan",
+            "Audi",
+            "60 dots",
+            "Elon Musk",
+            "Go Further",
+            "2002",
+            "England",
+            "Waltmart",
+            "France",
+            "Application Programming interface",
+            "Elon Musk",
+            "China"
+
+    };
+}
